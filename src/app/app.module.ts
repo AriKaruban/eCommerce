889ee3c2a08194d38f41185d9c2019c3f106607d
a_calvin <alexandra.calvin@davey.com>
2021-12-09 09:53:18 -0500

    background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooditemComponent } from './fooditem/fooditem.component';
+import { CartitemComponent } from './cartitem/cartitem.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooditemComponent,
+    CartitemComponent,
+    MenuComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fooditem/fooditem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fooditem',
+  templateUrl: './fooditem.component.html',
+  styleUrls: ['./fooditem.component.scss']
+})
+export class FooditemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
