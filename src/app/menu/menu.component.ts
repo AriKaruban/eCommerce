6a18ec35bdcdabe91dda5e7292b9d5ed10f84cc2
a_calvin <alexandra.calvin@davey.com>
2021-12-09 15:22:14 -0500

    typo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -14,7 +14,7 @@ export class MenuComponent implements OnInit {
     {name:"Salmon and Vegetables",price:4.5,img:'../assets/images/plate__salmon-vegetables.png'},
     {name:"Spaghetti with Meat Sauce",price:4.5,img:'../assets/images/plate__spaghetti-meat-sauce.png'},
   ]
-  colors=['#e1f0fe','#f7f7ff','#defef0',]
+  colors=['#e1f0fe','#f7f7ff','#defef0','#fbe4f0']
   constructor() { }
   colorpicker(){
     return this.colors[this.getRandomInt(this.colors.length)]
