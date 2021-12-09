984751add4ddc65fa241024f575dc799f6d18eaf
a_calvin <alexandra.calvin@davey.com>
2021-12-09 15:15:57 -0500

    menu design

--- src/app/fooditem/fooditem.component.ts
+++ src/app/fooditem/fooditem.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fooditem',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fooditem.component.scss']
 })
 export class FooditemComponent implements OnInit {
-
+  @Input() name
+  @Input() price
+  @Input() image
+  @Input() color
   constructor() { }
 
   ngOnInit(): void {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+  food=[
+    {name:"French Fries with Ketchup",price:4.5,img:'../assets/images/plate__french-fries.png'},
+    {name:"Salmon and Vegetables",price:4.5,img:'../assets/images/plate__salmon-vegetables.png'},
+    {name:"Spaghetti with Meat Sauce",price:4.5,img:'../assets/images/plate__spaghetti-meat-sauce.png'},
+    {name:"French Fries with Ketchup",price:4.5,img:'../assets/images/plate__french-fries.png'},
+    {name:"Salmon and Vegetables",price:4.5,img:'../assets/images/plate__salmon-vegetables.png'},
+    {name:"Spaghetti with Meat Sauce",price:4.5,img:'../assets/images/plate__spaghetti-meat-sauce.png'},
+  ]
+  colors=['#e1f0fe','#f7f7ff','#defef0',]
   constructor() { }
-
+  colorpicker(){
+    return this.colors[this.getRandomInt(this.colors.length)]
+  }
+  getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
   ngOnInit(): void {
   }
 
